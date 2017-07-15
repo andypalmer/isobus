58b5f259660266a73785c34d98b9c966aede9727
Andy Palmer and Antony Marcano <andy@andypalmer.com>
2017-07-15 16:10:27 +0100

    Compile and link the tests

---
+++ test_compile.zsh
@@ -0,0 +1,4 @@
+#!/usr/local/bin/zsh
+for f in tests/**/*.cpp; do
+  c++ -g -Wall -Werror -Wswitch-default -Wswitch-enum -Wno-unused-parameter -Wno-c99-extensions -Wno-keyword-macro -Wno-reserved-id-macro -Wno-c++11-long-long -Weverything -Wno-disabled-macro-expansion -Wno-padded -Wno-global-constructors -Wno-exit-time-destructors -Wno-weak-vtables -Wno-old-style-cast -include /usr/local/Cellar/cpputest/3.8/include/CppUTest/MemoryLeakDetectorNewMacros.h  -DCPPUTEST_COMPILATION -Wall -Werror -Wswitch-default -Wswitch-enum -Wno-unused-parameter -Wno-c99-extensions -Wno-keyword-macro -Wno-reserved-id-macro -Wno-c++11-long-long -include /usr/local/Cellar/cpputest/3.8/include/CppUTest/MemoryLeakDetectorMallocMacros.h  -I/usr/local/Cellar/cpputest/3.8/include/  -Iinclude/led_driver  -Iinclude/traffic_light    -c -MMD -MP -o objs/${f:r}.o ${f}
+done
