9c3909bcc4f9fb2dc03cebf430cb2667f2993c1a
Andy Palmer and Antony Marcano <andy@andypalmer.com>
2017-07-15 16:54:07 +0100

    We can generate an ID for a different message

--- tests/who_this_is.cpp
+++ tests/who_this_is.cpp
@@ -24,3 +24,25 @@ TEST(JoiningTheParty, tell_everyone_who_this_is ) {
 
   LONGS_EQUAL(0x18eeff03, canbus_id);
 }
+
+TEST(JoiningTheParty, tell_everyone_to_tell_me ) {
+  const unsigned int everyone = 0xff;
+  const unsigned long iso_request_pgn = 59904;
+
+  unsigned int priority = 6;
+  unsigned int extended_data_page = 0;
+  unsigned int data_page = 0;
+  unsigned long pgn = iso_request_pgn;
+  unsigned int destination_address = everyone;
+  unsigned int my_address = 3;
+
+  unsigned long canbus_id =
+      priority << 26
+    | extended_data_page << 25
+    | data_page << 24
+    | pgn << 8
+    | destination_address << 8
+    | my_address;
+
+  LONGS_EQUAL(0x18eaff03, canbus_id);
+}
