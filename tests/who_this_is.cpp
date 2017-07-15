afe5cc026a1e794c34a06ceb3207ad4c369fb8e2
Andy Palmer and Antony Marcano <andy@andypalmer.com>
2017-07-15 16:36:15 +0100

    Explain the magic numbers

--- tests/who_this_is.cpp
+++ tests/who_this_is.cpp
@@ -3,19 +3,22 @@
 TEST_GROUP(JoiningTheParty) {
 };
 
-TEST(JoiningTheParty, an_announcement ) {
+TEST(JoiningTheParty, tell_everyone_who_this_is ) {
+  const unsigned int everyone = 0xff;
+  const unsigned long iso_address_claim_pgn = 60928;
+
   unsigned int priority = 6;
   unsigned int extended_data_page = 0;
   unsigned int data_page = 0;
-  unsigned long iso_address_claim_pgn = 60928;
-  unsigned int destination_address = 0xff;
+  unsigned long pgn = iso_address_claim_pgn;
+  unsigned int destination_address = everyone;
   unsigned int my_address = 3;
 
   unsigned long canbus_id =
       priority << 26
     | extended_data_page << 25
     | data_page << 24
-    | iso_address_claim_pgn << 8
+    | pgn << 8
     | destination_address << 8
     | my_address;
 
